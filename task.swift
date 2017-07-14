b128973ac4b086dd858b531cc0d4717e96e6a8f7
Tony <piper1235@yahoo.com>
2017-07-14 16:49:03 -0700

    lists the tasks

---
+++ task.swift
@@ -0,0 +1,15 @@
+//
+//  task.swift
+//  DoIt1
+//
+//  Created by Tony on 7/14/17.
+//  Copyright © 2017 Tony. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+    
+}
